e03c8719f8be003349a6820af7c6c139ff51d007
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2022-03-30 22:22:02 +0530

    Add files

--- index.js
+++ index.js
@@ -6,71 +6,40 @@ const app = express()
 
 const newspapers = [
     {
-        name: 'cityam',
-        address: 'https://www.cityam.com/london-must-become-a-world-leader-on-climate-change-action/',
+        name: 'gktoday',
+        address: 'https://www.gktoday.in/current-affairs/',
         base: ''
     },
     {
-        name: 'thetimes',
-        address: 'https://www.thetimes.co.uk/environment/climate-change',
+        name: 'google news',
+        address: 'https://news.google.com/topstories?hl=en-IN&gl=IN&ceid=IN:en',
         base: ''
     },
-    {
-        name: 'guardian',
-        address: 'https://www.theguardian.com/environment/climate-crisis',
-        base: '',
-    },
-    {
-        name: 'telegraph',
-        address: 'https://www.telegraph.co.uk/climate-change',
-        base: 'https://www.telegraph.co.uk',
-    },
-    {
-        name: 'nyt',
-        address: 'https://www.nytimes.com/international/section/climate',
-        base: 'https://www.nytimes.com',
-    },
-    {
-        name: 'latimes',
-        address: 'https://www.latimes.com/environment',
-        base: '',
-    },
-    {
-        name: 'smh',
-        address: 'https://www.smh.com.au/environment/climate-change',
-        base: 'https://www.smh.com.au',
-    },
-    {
-        name: 'un',
-        address: 'https://www.un.org/climatechange',
-        base: '',
-    },
-    {
-        name: 'bbc',
-        address: 'https://www.bbc.co.uk/news/science_and_environment',
-        base: 'https://www.bbc.co.uk',
-    },
-    {
-        name: 'es',
-        address: 'https://www.standard.co.uk/topic/climate-change',
-        base: 'https://www.standard.co.uk'
-    },
-    {
-        name: 'sun',
-        address: 'https://www.thesun.co.uk/topic/climate-change-environment/',
-        base: ''
-    },
-    {
-        name: 'dm',
-        address: 'https://www.dailymail.co.uk/news/climate_change_global_warming/index.html',
-        base: ''
-    },
-    {
-        name: 'nyp',
-        address: 'https://nypost.com/tag/climate-change/',
-        base: ''
-    }
+    // {
+    //     name: 'thetimes',
+    //     address: 'https://www.thetimes.co.uk/environment/climate-change',
+    //     base: ''
+    // },
+    // {
+    //     name: 'guardian',
+    //     address: 'https://www.theguardian.com/environment/climate-crisis',
+    //     base: '',
+    // },
 ]
+
+
+// Working piece of code
+// $('a:contains("s")', html).each(function () {
+//     const title = $(this).text()
+//     const url = $(this).attr('href')
+
+//     articles.push({
+//         id: id++,
+//         title,
+//         url: newspaper.base + url,
+//         source: newspaper.name
+//     })
+// })
 
 const articles = []
 let id = 1
@@ -81,14 +50,14 @@ newspapers.forEach(newspaper => {
             const html = response.data
             const $ = cheerio.load(html)
 
-            $('a:contains("climate")', html).each(function () {
+            $('p:contains("k")', html).each(function () {
                 const title = $(this).text()
-                const url = $(this).attr('href')
+                // const url = $(this).attr('href')
 
                 articles.push({
                     id: id++,
                     title,
-                    url: newspaper.base + url,
+                    // url: newspaper.base + url,
                     source: newspaper.name
                 })
             })
@@ -96,8 +65,15 @@ newspapers.forEach(newspaper => {
         })
 })
 
+articles.push({
+    id: 10,
+    title: "Rein Welder appointed as CEO of Rein Corps",
+    url: "www.google.com",
+    source: "gktoday"
+})
+
 app.get('/', (req, res) => {
-    res.json('Welcome to my Climate Change News API')
+    res.json('Welcome to my Current Affairs News API')
 })
 
 app.get('/news', (req, res) => {
